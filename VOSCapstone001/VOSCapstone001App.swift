154ebcb29752ed9f0d02ccdad9ba60985fb033f6
James Nachbar <dr@nach.bar>
2024-06-13 20:03:35 -0700

    Initial Commit

---
+++ VOSCapstone001/VOSCapstone001App.swift
@@ -0,0 +1,33 @@
+//
+//  VOSCapstone001App.swift
+//  VOSCapstone001
+//
+//  Created by James Nachbar on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct VOSCapstone001App: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
---
+++ VOSCapstone001Tests/VOSCapstone001Tests.swift
@@ -0,0 +1,17 @@
+//
+//  VOSCapstone001Tests.swift
+//  VOSCapstone001Tests
+//
+//  Created by James Nachbar on 6/13/24.
+//
+
+import Testing
+@testable import VOSCapstone001
+
+struct VOSCapstone001Tests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

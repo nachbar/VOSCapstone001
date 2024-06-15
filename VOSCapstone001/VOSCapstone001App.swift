0e295cd848972624c8087b1b4272c557f26a169c
James Nachbar <dr@nach.bar>
2024-06-14 19:08:09 -0700

    got immersive space to dismiss if user closes the volume with the close button

--- VOSCapstone001/VOSCapstone001App.swift
+++ VOSCapstone001/VOSCapstone001App.swift
@@ -7,15 +7,45 @@
 
 import SwiftUI
 
+
 @main
 struct VOSCapstone001App: App {
 
+    @Environment(\.dismissImmersiveSpace) private var dismissImmersiveSpace
+    @Environment(\.scenePhase) private var scenePhase
     @State private var appModel = AppModel()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(appModel)
+            // .onAppear always gets called, but .onDisappear doesn't.  onChange(of: scenePhase) allows us to detect when the ContentView is closed, and escape from immersive view
+            .onChange(of: scenePhase) { _, newScenePhase in
+                if newScenePhase == .background || newScenePhase == .inactive {
+                    //appState.visibleScenes.remove(trackedScene)
+                    Task { @MainActor in
+                        switch appModel.immersiveSpaceState {
+                            case .open:
+                                appModel.immersiveSpaceState = .inTransition
+                            print("about to await dismissImmersiveSpace")
+                                await dismissImmersiveSpace()
+                                // Don't set immersiveSpaceState to .closed because there
+                                // are multiple paths to ImmersiveView.onDisappear().
+                                // Only set .closed in ImmersiveView.onDisappear().
+
+                            break
+
+                            case .inTransition:
+                                // This case should not ever happen because button is disabled for this case.
+                                break
+                        case .closed:
+                            break
+                        }
+                    }
+                    
+                }
+                print("onChange(of: scenePhase) called for ContentView in App, with scenePhase \(scenePhase), newScenePhase \(newScenePhase)")
+            }
         }
         .windowStyle(.volumetric)
 
